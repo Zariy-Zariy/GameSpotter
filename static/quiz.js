cff66f5c3484cb27970cd1ba238b81e048fd16cb
bob <zaroui.iyad@hotmail.com>
2023-07-09 14:05:45 +0200

    started making the buttons work

--- static/quiz.js
+++ static/quiz.js
@@ -1,10 +1,15 @@
-// Make the button go blue when it's selected
 
-let answers = document.querySelectorAll(".answer-grid");
-let inputs_answer = document.querySelectorAll(".answer-grid input");
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    for(answer of answers){
-        alert(answer)
+document.addEventListener("DOMContentLoaded", function() {
+    // Make the button go blue when it's selected
+    let inputs_answer = document.querySelectorAll(".answer-grid input");
+    let last_selected;
+
+    for(let i = 0; i < inputs_answer.length; i++){
+        inputs_answer[i].addEventListener("change", function(){
+
+            inputs_answer[i].closest(".answer-grid").style.backgroundColor = "blue";
+            
+        });
     }
 });
