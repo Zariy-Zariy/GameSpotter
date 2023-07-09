a567bc9e7a75426bf83ae1604ca882183137262a
bob <zaroui.iyad@hotmail.com>
2023-07-09 12:47:13 +0200

    Started redoing the quiz webpage because it was horrible

---
+++ static/quiz.js
@@ -0,0 +1,10 @@
+// Make the button go blue when it's selected
+
+let answers = document.querySelectorAll(".answer-grid");
+let inputs_answer = document.querySelectorAll(".answer-grid input");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    for(answer of answers){
+        alert(answer)
+    }
+});
